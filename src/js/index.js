18cb255ec8fbd78a3f32fd2f744c3d51f048072b
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-05-18 18:21:55 +0200

    add new task modification

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,6 @@ const renderToDo = collection => {
             <p>Termin: ${todo.due_date}</p>
             <p>Priorytet: ${todo.priority}</p>
             <p>Completed: ${todo.completed}</p>
-            <a href="/add.html">Add</a>
             <a href="/edit.html?id=${todo.id}">Edit</a>
             <a href="/detail.html?id=${todo.id}">Details</a>
             <a href="/delete.html?id=${todo.id}">Delete</a>
